5b046be40c5bd04bab24d8d6cc8f9f9e8db20b0f
satwik073 <satwikkanhere2003@gmail.com>
2026-02-12 21:23:38 +0530

    feat: Add `projectId` argument to `openFile` calls in the source control panel.

--- src/inngest/client.ts
+++ src/inngest/client.ts
@@ -4,5 +4,6 @@ import { sentryMiddleware } from "@inngest/middleware-sentry";
 // Create a client to send and receive events
 export const inngest = new Inngest({ 
   id: "flux",
+  isDevelopment: false,
   middleware: [sentryMiddleware()],
 });
